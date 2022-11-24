fc63e8df7aed65f061019eadfa6720e95e05e740
Lekz <koshanskiy00@mail.ru>
2022-11-24 03:29:58 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
